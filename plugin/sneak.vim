justinmk/vim-sneak

commit f1eb049319bffdda94c5cfeb5c40b603ee9e529f

sneak#to(): Nudge cursor instead of using visual-mode.
Instead of using visual-mode to make sure the character under the
cursor is included, just move the cursor to the right (or left).

Fixes #177, because a visual selection is always at least 1 character.

Also makes using operator-pending mode icm with streak-mode look a
little less ugly :)

plugin/sneak.vim
@@ -144,10 +144,6 @@ func! sneak#to(op, input, inputlen, count, repeatmotion, reverse, inclusive, str
     call s:ft_hook()
   endif
 
-  if is_op && 2 != a:inclusive && !a:reverse
-    norm! v
-  endif
-
   let nextchar = searchpos('\_.', 'n'.(s.search_options_no_s))
   let nudge = !a:inclusive && a:repeatmotion && nextchar == s.dosearch('n')
   if nudge
@@ -206,9 +202,14 @@ func! sneak#to(op, input, inputlen, count, repeatmotion, reverse, inclusive, str
     nmap <expr> <silent> <esc> sneak#cancel() . "\<esc>"
   endif
 
-  "enter streak-mode iff there are >=2 _additional_ on-screen matches.
+  " Operators always invoke streak-mode; also for 3+ on-screen matches.
   let target = (2 == a:streak || (a:streak && g:sneak#opt.streak && (is_op || s.hasmatches(2)))) && !max(bounds)
         \ ? sneak#streak#to(s, is_v, a:reverse) : ""
+
+  if is_op && 2 != a:inclusive && !a:reverse
+    " f/t operations do not apply to the current character; nudge the cursor.
+    call sneak#util#nudge(1)
+  endif
 
   if is_op || "" != target
     call sneak#hl#removehl()

commit 5a3aec1f0511e0f63c02b023a681c986f551730e

fix: use visual mode for operator-pending mode f/t
f1eb049319bffdda94c5cfeb5c40b603ee9e529f (fix for #177) broke
operator-pending mode f when the target is on eol/eof (#291). Revert
that commit in order to fix #291, and introduce an alternative fix for
 #177: exit visual mode if target is not found.

Fixes #291, #177

plugin/sneak.vim
@@ -164,6 +164,11 @@ func! sneak#to(op, input, inputlen, count, register, repeatmotion, reverse, incl
     call s:ft_hook()
   endif
 
+  let in_visual = is_op && 2 != a:inclusive && !a:reverse
+  if in_visual
+    norm! v
+  endif
+
   let nextchar = searchpos('\_.', 'n'.(s.search_options_no_s))
   let nudge = !a:inclusive && a:repeatmotion && nextchar == s.dosearch('n')
   if nudge
@@ -180,6 +185,9 @@ func! sneak#to(op, input, inputlen, count, register, repeatmotion, reverse, incl
   endfor
 
   if 0 == max(matchpos)
+    if in_visual
+      exe "norm! \<esc>"
+    endif
     if nudge
       call sneak#util#nudge(a:reverse) "undo nudge for t
     endif
@@ -230,11 +238,6 @@ func! sneak#to(op, input, inputlen, count, register, repeatmotion, reverse, incl
 
   if nudge
     call sneak#util#nudge(a:reverse) "undo nudge for t
-  endif
-
-  if is_op && 2 != a:inclusive && !a:reverse
-    " f/t operations do not apply to the current character; nudge the cursor.
-    call sneak#util#nudge(1)
   endif
 
   if is_op || '' != target